5be47519257ff0b4286b94338edf834e7cc24801
Benjamin Poon <benjamintpoon@gmail.com>
2021-08-14 23:22:25 -0500

    todos in angular

---
+++ todos/angular/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // State
+  todos: string[] = []
+  todoText = ''
+
+  // Lifecycle
+  ngOnInit() {
+    const existingTodos = localStorage.getItem('todos')
+    this.todos = JSON.parse(existingTodos as string) || []
+  }
+
+  //Events
+  addTodo() {
+    this.todos.push(this.todoText)
+    localStorage.setItem('todos', JSON.stringify(this.todos))
+  }
+}
