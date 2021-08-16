faaabae5989224687a1c64d8fb35803644a65f66
Benjamin Poon <benjamintpoon@gmail.com>
2021-08-16 02:17:53 -0500

    todos in Vue

---
+++ todos/vue/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo">{{ todo }}</li>
+      <form v-on:submit.prevent="addTodo">
+        <input v-model="todoText" placeholder="What needs to be done?" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function () {
+    return {
+      todos: [],
+      todosText: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      this.todos = [...this.todos, this.todoText];
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+  mounted: function () {
+    const existingTodos = localStorage.getItem("todos");
+    this.todos = JSON.parse(existingTodos) || [];
+  },
+};
+</script>
+
+<style scoped>
+</style>
