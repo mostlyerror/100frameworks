1eb7590497aeba494c301333767edd536ff5ccc4
Benjamin Poon <benjamintpoon@gmail.com>
2021-08-17 01:15:54 -0500

    todos in svelte

---
+++ todos/svelte/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  let todos = []
+  let todoText = ''
+
+  onMount(() => {
+    const existingTodos = localStorage.getItem('todos')
+    todos = JSON.parse(existingTodos) || []
+  })
+
+  function addTodo() {
+    todos = [...todos, todoText]
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+</script>
+
+<main>
+  <ul>
+    {#each todos as todo}
+      <li>{todo}</li>
+    {/each}
+  </ul>
+
+  <form on:submit|preventDefault={addTodo}>
+    <input bind:value={todoText} />
+    <input type="submit" value="Add todo" />
+  </form>
+</main>
